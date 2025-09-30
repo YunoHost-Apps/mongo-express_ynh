5793b845d1f45b75f846983e0b46767b7a42f54a
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-09-30 12:29:30 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,4 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version=20
-mongo_version=7.0
-
-_install_yarn () {
-    ynh_exec_as_app $nodejs_dir/npm install yarn
-    ynh_yarn="$install_dir/node_modules/.bin/yarn"
-    alias ynh_yarn="$ynh_yarn"
-}
+mongo_version="7.0"
