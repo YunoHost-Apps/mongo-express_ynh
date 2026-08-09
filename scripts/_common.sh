f269345fadd6e0df909764591cbc37fb224c710e
Gérard Collin <gerard.collin@appliweb.net>
2026-08-09 10:18:28 +0200

    upgrade to 1.1.0-rc4 pre-built packages

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,3 @@
 #=================================================
 
 mongo_version=7.0
-
-_install_yarn () {
-    ynh_exec_as_app $nodejs_dir/npm install yarn
-    ynh_yarn="$install_dir/node_modules/.bin/yarn"
-    alias ynh_yarn="$ynh_yarn"
-}
